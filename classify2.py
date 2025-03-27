03263bafa0713f4a4be671747fa4f520ba930b06
Dietrich Kappe <dkappe@gmail.com>
2025-03-27 12:31:18 -0500

    added pth model

--- classify2.py
+++ classify2.py
@@ -123,10 +123,13 @@ if __name__ == "__main__":
 
     # Load the model
     #device = "cuda" if torch.cuda.is_available() else "cpu"
-    device = "cpu"
     model = CR2NNModel(num_classes=2)
-    checkpoint = torch.load(args.checkpoint, map_location=device)
-    model.load_state_dict(checkpoint["state_dict"])  # Adjust if using standard PyTorch checkpoint
+    model.load_state_dict(torch.load("model/model.pth", map_location="cpu"))
+    model.eval()
+    device = "cpu"
+    #model = CR2NNModel(num_classes=2)
+    #checkpoint = torch.load(args.checkpoint, map_location=device)
+    #model.load_state_dict(checkpoint["state_dict"])  # Adjust if using standard PyTorch checkpoint
     model = model.to(device)
 
     # List only .txt files
